a283c39027a9154811c2eedbec36e03379ecff35
Broseph <josephpetitt102@gmail.com>
2025-01-28 17:11:29 -0500

    added return of book list when base get route called, also added get book by isbn

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -5,27 +5,95 @@ let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
 
+const doesExist = (username) => {
+
+    let userswithsamename = users.filter((user) => {
+
+        return user.username === username;
+    });
+
+    if (userswithsamename.length > 0) {
+
+        return true;
+    } else {
+
+        return false;
+    }
+
+}
+
+
 public_users.post("/register", (req,res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (username && password) {
+
+
+    if(!doesExist(username)) {
+
+
+        users.push({'username': username, 'password': password});
+        return res.status(200).json({message: "User successfully registered :)!"});
+    } else {
+
+        return res.status(404).json({message: 'User already exists!'});
+    }
+
+  }
+
+  return res.status(404).json({message: "Registration Failed :("});
 });
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  
+  let booksJsonString = JSON.stringify(books, null, 4);
+
+  res.send(booksJsonString)
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+
+  const isbn = req.params.isbn;
+
+  if(isbn) {
+    const book = books[isbn];
+    if(book) {
+        return res.send(JSON.stringify(book, null,4));
+        
+    } else {
+
+        return res.status(404).send({ message: "Book of ISBN " + isbn + " does not exist in our db"});
+
+    }
+
+  } 
+  
+  return res.status(404).json({message: "Bad Request Home dawg"});
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+   
+    const author = req.params.author;
+
+    if(isbn) {
+      const book = books[isbn];
+      if(book) {
+          return res.send(JSON.stringify(book, null,4));
+          
+      } else {
+  
+          return res.status(404).send({ message: "Book of ISBN " + isbn + " does not exist in our db"});
+  
+      }
+  
+    } 
+    
+    return res.status(404).json({message: "Bad Request Home dawg"});
 });
 
 // Get all books based on title
