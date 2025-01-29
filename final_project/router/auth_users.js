e01e10caf7c1d6fa11dad0d8b45ed3af83d2844e
Broseph <josephpetitt102@gmail.com>
2025-01-28 19:22:01 -0500

    added create review functionality

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -60,8 +60,32 @@ regd_users.post("/login", (req,res) => {
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  const review = req.query.review;
+
+  const username = req.session.authorization.username;
+
+if (!review) {
+
+    return res.status(400).json({message: "Review text is required baybay"});
+
+}
+
+if (!books[isbn].reviews) {
+    books[isbn].reviews = {};
+}
+
+books[isbn].reviews[username] = review;
+
+return res.status(200).json({
+    message: "Review successfully posted",
+    reviews: books[isbn].reviews
 });
+
+ 
+});
+
+
 
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
